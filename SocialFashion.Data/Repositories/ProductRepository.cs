58a89af5c48a4e963c34961b9202a99bc2fbc350
maithanhdat1996 <mtdat1996>
2016-12-24 09:42:26 +0700

    đây là bản khác nhé. Đã có Hub server. Mọi người get bản này về nhé

---
+++ SocialFashion.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,31 @@
+﻿using SocialFashion.Data.Infrastructure;
+using SocialFashion.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+namespace SocialFashion.Data.Repositories
+{
+    
+    public interface IProductRepository : IRepository<Product>
+    {
+        IEnumerable<Product> TestGetAllProduct();
+    }
+    public class ProductRepository : RepositoryBase<Product>, IProductRepository
+    {
+        private SocialFashionDbContext db = new SocialFashionDbContext();
+        public ProductRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+
+        public IEnumerable<Product> TestGetAllProduct()
+        {
+            IEnumerable<GetAllProduct_Result> lstProductSp =  db.GetAllProduct().ToList();
+            Mapper.CreateMap<Product, GetAllProduct_Result>().ReverseMap();
+            IEnumerable<Product> listProduct = Mapper.Map<IEnumerable<Product>>(lstProductSp);
+            return listProduct;
+         }
+    }
+}
---
+++ SocialFashion.Web/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNet.Identity;
+using SocialFashion.Model.Models;
+using SocialFashion.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SocialFashion.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        SocialFashionDbContext db;
+        public HomeController(IProductService productServicer)
+        {
+            db = new SocialFashionDbContext();
+        }
+
+        public ActionResult Index()
+        {
+            IEnumerable<GetAllStutusByUserId_Result> list = db.GetAllStutusByUserId(User.Identity.GetUserId()).ToList<GetAllStutusByUserId_Result>();
+            return View(list);
+        }
+
+        public ActionResult Chat()
+        {
+            return PartialView();
+        }
+
+        public ActionResult ToolbarDesktopPartial()
+        {
+            return PartialView();
+        }
+
+        public JsonResult GetNotificationAPI()
+        {
+            
+            NotificationComponent NC = new NotificationComponent();
+            var list = NC.GetNotification();
+      
+            return new JsonResult { Data = list,JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+        public JsonResult GetCountNotificationAPI()
+        {
+
+            NotificationComponent NC = new NotificationComponent();
+            var count = NC.GetCountNotification();
+
+            return new JsonResult { Data = count, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+    }
+}
